464cbf849018c3a9721794f106edc17d70d1b6c5
DELL <DELL@DESKTOP-LFRNAK6>
2023-07-18 15:32:57 +0530

    First Commit

---
+++ whileloop/src/whileloop/demo1.java
@@ -0,0 +1,16 @@
+package whileloop;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1,n;
+		while(i<=10) {
+			n=2*i;
+			System.out.println("2 X "+i+"="+n);
+			i++;
+		}
+
+	}
+
+}
